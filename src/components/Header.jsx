ab94520cf608d1b2908dd4e59ac0653d2bbf9f9d
scorp <vijayalamin@gmail.com>
2022-07-16 09:33:27 +0600

    products file structure header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div class="ui fixed menu">
+            <div class="ui container center">
+                <h2>RED CART</h2>
+            </div>
+        </div>
     )
 }
--- src/components/ProductCart.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ProductCart() {
-  return (
-    <div>ProductCart</div>
-  )
-}
